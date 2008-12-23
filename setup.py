5cca5b16a0719fc301ab3d5835fb09d06d54105e
dugang <dugang@188.com>
2008-12-23 08:38:09 +0000

    add setup.py for py2exe.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+#############################################################################
+#                                                                           #
+#   File: setup.py                                                          #
+#                                                                           #
+#   Copyright (C) 2008 Du XiaoGang <dugang@188.com>                         #
+#                                                                           #
+#   This file is part of UDPonNAT.                                          #
+#                                                                           #
+#   UDPonNAT is free software: you can redistribute it and/or modify        #
+#   it under the terms of the GNU General Public License as                 #
+#   published by the Free Software Foundation, either version 3 of the      #
+#   License, or (at your option) any later version.                         #
+#                                                                           #
+#   UDPonNAT is distributed in the hope that it will be useful,             #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#   GNU General Public License for more details.                            #
+#                                                                           #
+#   You should have received a copy of the GNU General Public License       #
+#   along with UDPonNAT.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                           #
+#############################################################################
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['stunclient.py', 'server.py', 'client.py', 'cernet.py'])
